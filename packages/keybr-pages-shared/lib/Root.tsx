a75ae5443af34573c30836bbdad99e54a4422d58
congyuemao <congyuemao0304@gmail.com>
2026-06-29 08:39:22 +1000

    Use static root id in Root component

--- packages/keybr-pages-shared/lib/Root.tsx
+++ packages/keybr-pages-shared/lib/Root.tsx
@@ -1,12 +1,11 @@
 import { type ReactNode } from "react";
-import * as styles from "./Root.module.less";
 
 export function Root({
   children,
 }: {
   readonly children?: ReactNode;
 }): ReactNode {
-  return <div id={styles.root}>{children}</div>;
+  return <div id="root">{children}</div>;
 }
 
-Root.selector = `#${styles.root}`;
+Root.selector = "#root";
